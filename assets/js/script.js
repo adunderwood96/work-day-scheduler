dc1e98f6cee98ef7cfa9d1c0df0da76ce82674dd
Ashley <adunderwood96@gmail.com>
2021-11-28 19:19:25 -0500

    final updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 // Time Variables (time by 24hr clock)
 
-// current time shown in header and add time blocks to color (past/pres/future)
+// current time shown in header
 var timeEl = $("#currentDay");
 var currentTime;
 timeClock();
-//Load data from local storage on start
+//Load data from start
 loadData();
 
 //Set interval to constantly update time
 var clock = setInterval(timeClock, 1600)
 
-//Set current time and check time blocks
-function timeClock(){
+//Set current time to user local time and check time blocks
+function timeClock() {
     currentTime = moment();
     timeEl.text(currentTime);
     checkTimeBlock();
 }
 
 // coordinate the color of time blocks based on current time
-function checkTimeBlock(){
+function checkTimeBlock() {
     var currentHour = currentTime.hours();
     var timeBlock = $(".time-block");
-    for(var i = 0; i < timeBlock.length; i++){
+    for (var i = 0; i < timeBlock.length; i++) {
         var block = timeBlock[i];
-        if(parseInt(block.id.split("-")[0]) < currentHour){
+        if (parseInt(block.id.split("-")[0]) < currentHour) {
             $(block).addClass("past");
         }
-        else if(parseInt(block.id.split("-")[0]) === currentHour){
+        else if (parseInt(block.id.split("-")[0]) === currentHour) {
             $(block).removeClass("past");
             $(block).addClass("present");
         }
-        else{
+        else {
             $(block).removeClass("past");
             $(block).removeClass("present");
             $(block).addClass("future");
@@ -38,39 +38,39 @@ function checkTimeBlock(){
     }
 }
 
-//Save button click listener
+//Save button click listener/function
 $(".saveBtn").on("click", handleSaveClick);
 
-function handleSaveClick(e){
+function handleSaveClick(e) {
     var text = $(e.target).siblings(".description").val();
     var time = $(e.target).parent().attr("id");
 
-    if(text === "")
+    if (text === "")
         alert("To save your task the text input field must not be blank!")
-    else{
+    else {
         localStorage.setItem(time, text);
         alert("Task has been saved");
     }
 
 }
 
-//Clear button click listener
+//Clear button click listener/function
 $(".clearBtn").on("click", handleClearClick);
 
 //Save input to local storage
-function handleClearClick(e){
-    
+function handleClearClick(e) {
+
     var clear = confirm("Click ok if you would like to clear the schedule");
 
-    if(clear){
+    if (clear) {
         localStorage.clear();
         loadData();
     }
-    
+
 }
 
-//Load data from local storage to each time block (24hr clock)
-function loadData(){ 
+//local storage for each time block (24hr clock) starting at 7am-9pm
+function loadData() {
     $('#7 .description').val(localStorage.getItem('7'));
     $('#8 .description').val(localStorage.getItem('8'));
     $('#9 .description').val(localStorage.getItem('9'));
